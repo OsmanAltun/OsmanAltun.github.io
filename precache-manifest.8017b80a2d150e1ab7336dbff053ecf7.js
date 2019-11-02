a81bc5ac443ed9f1188cd3352ebd80f08c43de86
BuildTools <unconfigured@null.spigotmc.org>
2019-11-02 15:11:22 +0100

    Updates

---
+++ precache-manifest.8017b80a2d150e1ab7336dbff053ecf7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f30b8809771bf391e5cadb83e30670",
+    "url": "osmanaltun.github.com/index.html"
+  },
+  {
+    "revision": "3705a9f26c96de6ea15f",
+    "url": "osmanaltun.github.com/static/css/main.fecce0d5.chunk.css"
+  },
+  {
+    "revision": "edffe35e8ee20821612a",
+    "url": "osmanaltun.github.com/static/js/2.bd85d120.chunk.js"
+  },
+  {
+    "revision": "3705a9f26c96de6ea15f",
+    "url": "osmanaltun.github.com/static/js/main.5606752b.chunk.js"
+  },
+  {
+    "revision": "c8de96c05debc741014c",
+    "url": "osmanaltun.github.com/static/js/runtime~main.261f26d6.js"
+  },
+  {
+    "revision": "3d99af0785953172d586e6a3ed20a005",
+    "url": "osmanaltun.github.com/static/media/CV.3d99af07.svg"
+  },
+  {
+    "revision": "0e5c42f66563ea11d5f3652ff61ea290",
+    "url": "osmanaltun.github.com/static/media/contact.0e5c42f6.jpg"
+  },
+  {
+    "revision": "db087221e764d236cdc315ccee57b52f",
+    "url": "osmanaltun.github.com/static/media/curriculumVitae.db087221.pdf"
+  },
+  {
+    "revision": "2d368649c68018f813c6f810b9e4c57c",
+    "url": "osmanaltun.github.com/static/media/ecg.2d368649.png"
+  },
+  {
+    "revision": "b1b7a419365830b0788b45dc659614e3",
+    "url": "osmanaltun.github.com/static/media/email.b1b7a419.svg"
+  },
+  {
+    "revision": "eede1161d539ab58c57344456e3f4974",
+    "url": "osmanaltun.github.com/static/media/flappyBird.eede1161.png"
+  },
+  {
+    "revision": "45b4b72c778a42ad5ea487bdb68bc3d6",
+    "url": "osmanaltun.github.com/static/media/github.45b4b72c.svg"
+  },
+  {
+    "revision": "7b3cee3354d1916843cd341fcf056643",
+    "url": "osmanaltun.github.com/static/media/header.7b3cee33.jpg"
+  },
+  {
+    "revision": "ecf33dd2d0585127a27f807583bad872",
+    "url": "osmanaltun.github.com/static/media/hro.ecf33dd2.jpg"
+  },
+  {
+    "revision": "370cf3890a782cfb01923cd66ba0b2d7",
+    "url": "osmanaltun.github.com/static/media/linkedin.370cf389.svg"
+  },
+  {
+    "revision": "7dcda859de66f1b848cf5d1bc7a15e47",
+    "url": "osmanaltun.github.com/static/media/musicClassification.7dcda859.png"
+  },
+  {
+    "revision": "314fdb859399e23f5d66be449cdd08e4",
+    "url": "osmanaltun.github.com/static/media/recruiter.314fdb85.png"
+  },
+  {
+    "revision": "487bcde2fbe942ca5591d668848276b3",
+    "url": "osmanaltun.github.com/static/media/stolenBikes.487bcde2.png"
+  },
+  {
+    "revision": "8c55bf9c70fdcc415c0ddb05e286d291",
+    "url": "osmanaltun.github.com/static/media/towerClimbingQuiz.8c55bf9c.png"
+  },
+  {
+    "revision": "f1a3d8c5fe40b645ae459570f500b970",
+    "url": "osmanaltun.github.com/static/media/webshop.f1a3d8c5.png"
+  }
+]);
